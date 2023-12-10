96e4cf2d754cc57ab497a1f86a911c285ee5d873
04M04 <micsch04@gmail.com>
2023-12-10 21:50:33 +0100

    finished 04

--- main-04-1.py
+++ main-04-1.py
@@ -1,7 +1,11 @@
-f = open("aoc04TestText.txt")
+import numpy as np
+
+# f = open("aoc04TestText.txt")
+f = open("aoc04Text.txt")
 
 # print(f.read())
 cards = []
+result = 0
 
 for l in f:
     data1,data2 = l.split(": ")[1].split(" | ")
@@ -13,8 +17,20 @@ for l in f:
         data2 = data2[1:]
     cards.append([data1,data2])
 
-for elem in cards:
-    print(elem)
+for card in cards:
+    nix = np.isin(card[0], card[1])
+    print(nix)
+    counted = np.count_nonzero(nix)
+    print(counted)
+    if counted > 0:
+        res = 1
+        for i in range(counted-1):
+            res *= 2
+        result += res
+    print(res)
+
+print(result)
+          
 
 
 # dataDict = [[None][None]]
