cad64e7251fe367e2330805f0f17de19f2d96d11
04M04 <micsch04@gmail.com>
2023-12-11 04:36:32 +0100

    just a safe

--- python-08/main-2.py
+++ python-08/main-2.py
@@ -4,38 +4,44 @@ import numpy as np
 f = open("text-1.txt")
 
 # print(f.read())
-cards = []
 result = 0
 
-time = f.readline().split(": ")[1].replace("\n", "").replace(" ", "")
-dist = f.readline().split(": ")[1].replace("\n", "").replace(" ", "")
+directions = f.readline().replace("\n", "")
+positions = []
+
+f.readline()
+for l in f:
+    # print(l)
+    pos,data = l.split(" = ")
+    left,right = data.replace("\n", "").replace("(", "").replace(")", "").split(", ")
+    positions.append([pos,left,right])
 
 
+startPoses = [elem[0] for elem in positions if elem[0][2] == "A"]
+for elem in startPoses:
+    print(f"startPoses: {elem[2]}")
 
-# times = [elem for elem in times if elem != ""]
-# dists = [elem for elem in dists if elem != ""]
-
-print(f"time: {time}")
-print(f"dist: {dist}")
-
-# data = []
-# for i in range(len(times)):
-#     data.append([times[i], dists[i]])
-
-# print(f"data: {data}")
-
-# results = []
-
-
-for waitTime in range(int(time)):
-    res = (float(time) - waitTime) * waitTime
-
-    if res > float(dist):
+lastPaths = startPoses
+print(all([False for elem in lastPaths if elem[2] == "Z"]))
+while(all([False for elem in lastPaths if elem[2] == "Z"])):
+    for dir in directions:
+        # foundPaths = [pos for pos in positions if pos[0] == lastPaths]
+        # print(foundPaths)
+        for lastPath in lastPaths:
+            # print(lastPaths)
+            # print(f"lastPath: {lastPath}")
+            # print([elem for elem in positions if elem[2] == lastPath])
+            foundCorner = [pos for pos in positions if pos[0] == lastPath][0]
+            
+            # print(f"newpath: {foundCorner}")
+            if dir == "L":
+                lastPath = foundCorner[0][1]
+            else:
+                lastPath = foundCorner[0][2]
         result += 1
-
-
-
-
+        if all([False for el in lastPaths if el[2] == "Z"]):
+            break
+    
 print(f"result: {result}")
 
 
